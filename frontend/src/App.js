513fb264e05c053a29ddd91d2bcecf7fb7c0425e
Yashavanth H S <108397456+VIRTUOSOCRYPTO@users.noreply.github.com>
2026-01-21 21:35:58 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,25 @@ import HomePage from "@/pages/HomePage";
 import AnalysisPage from "@/pages/AnalysisPage";
 import ResultsPage from "@/pages/ResultsPage";
 import { Toaster } from "@/components/ui/sonner";
+import { AccessibilityProvider } from "@/context/AccessibilityContext";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/analyze" element={<AnalysisPage />} />
-          <Route path="/results/:reportId" element={<ResultsPage />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
-    </div>
+    <LanguageProvider>
+      <AccessibilityProvider>
+        <div className="App">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/analyze" element={<AnalysisPage />} />
+              <Route path="/results/:reportId" element={<ResultsPage />} />
+            </Routes>
+          </BrowserRouter>
+          <Toaster />
+        </div>
+      </AccessibilityProvider>
+    </LanguageProvider>
   );
 }
 
